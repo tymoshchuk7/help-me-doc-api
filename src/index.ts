07c3d4c80c0313bf21652648d3b8f1f605f609de
otymoshchuk <otymoshchuk@wemyit.com>
2023-04-07 15:08:43 +0300

    feature: install cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-import express from 'express';
+import express, { json } from 'express';
+import cors from 'cors';
 import { initGlobalTables } from './database';
 
 const app = express();
+
+app.disable('x-powered-by');
+app.use(json());
+app.use(cors({ origin: 'http://localhost:3000' }));
+
 const PORT = process.env.PORT || 8000;
 
 export const bootstrap = async () => {
