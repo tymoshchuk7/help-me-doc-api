7b99b58a924ccf7799b7fe500f189da6e2b731ca
otymoshchuk <otymoshchuk@wemyit.com>
2023-07-22 23:10:11 +0300

    feat: add select of participant to get me endpoint

--- app/routes/users/me.ts
+++ app/routes/users/me.ts
@@ -1,5 +1,18 @@
 import { Response, Request } from 'express';
+import { asyncRoute, prepareAPIResponse } from '../../helpers';
+import { ParticipantController } from '../../controllers';
 
-export default function (req: Request, res: Response) {
-  return res.json({ user: req.user });
-}
+export default asyncRoute(async (req: Request, res: Response) => {
+  const { user } = req;
+
+  const participant = await ParticipantController.findOne(user.defaultTenant, { user_id: user.id });
+
+  if (!participant) {
+    throw new Error('Participant is missing');
+  }
+
+  return res.json({
+    user: req.user,
+    participants: prepareAPIResponse(participant),
+  });
+});
