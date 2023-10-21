b7c7347438729d6f3b79fc3acc8191d7aa108738
otymoshchuk <otymoshchuk@wemyit.com>
2023-10-22 00:16:38 +0300

    feat: add logging middleware

--- app/index.ts
+++ app/index.ts
@@ -1,12 +1,14 @@
 import express, { json } from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import routes from './routes';
 
 const app = express();
 
 app.disable('x-powered-by');
-app.use(json());
 app.use(cors({ origin: process.env.APP_URL }));
+app.use(json());
+app.use(morgan('tiny'));
 app.use('/v1', routes);
 
 const PORT = process.env.PORT || 8000;
