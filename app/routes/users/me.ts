52c31468bae047eb60d1646ff0a7faa8b808ea38
otymoshchuk <otymoshchuk@wemyit.com>
2024-01-05 23:34:29 +0200

    feat: add tenant participants and user to 'me' endpoint response

--- app/routes/users/me.ts
+++ app/routes/users/me.ts
@@ -1,15 +1,22 @@
 import { Response, Request } from 'express';
 import { asyncRoute, prepareAPIResponse } from '../../helpers';
+import { ParticipantController, UserController } from '../../controllers';
+import { ModelMeta } from '../../types';
 
-export default asyncRoute( (req: Request, res: Response) => {
+export default asyncRoute( async (req: Request, res: Response) => {
   const { tenantParticipant } = req;
+  const { user } = req;
 
   if (!tenantParticipant) {
     throw new Error('Participant is missing');
   }
 
+  const tenantParticipants = await ParticipantController.find(user.defaultTenant, {});
+  const users = await UserController.find({});
+
   return res.json({
-    user: req.user,
-    participants: prepareAPIResponse(tenantParticipant),
+    user,
+    participants: prepareAPIResponse(tenantParticipants as ModelMeta[]),
+    users: prepareAPIResponse(users as ModelMeta[]),
   });
 });
