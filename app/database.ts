00ca51e7dcfdc721d32a8a688c4d613e2b4208e5
tymoshchuk7 <tymoshchukalex7@gmail.com>
2024-10-19 23:20:58 +0300

    feat: commit database connection file

---
+++ app/database.ts
@@ -0,0 +1,126 @@
+import { knex, Knex } from 'knex';
+import { getTenantTablesNames } from './helpers';
+import { config } from './config';
+import { Tenant, GlobalTableNames } from './types';
+
+export const db = knex( {
+  client: 'pg',
+  connection: {
+    host: config.dbHost,
+    port: config.dbPort,
+    database: config.dbName,
+    user: config.dbUser,
+    password: config.dbPassword,
+  },
+});
+
+export abstract class CreateTenantTables {
+  database: Knex<any, unknown[]>;
+
+  constructor(database: Knex<any, unknown[]>) {
+    this.database = database;
+  }
+
+  protected async createTenantParticipantsTable(tenant: Tenant, ctx: any) {
+    await this.database.schema.createTable(tenant.tenant_participants_table, (table) => {
+      table.uuid('id', { primaryKey: true }).defaultTo(this.database.raw('uuid_generate_v4()'));
+      table.uuid('user_id').references('users.id').onDelete('CASCADE');
+      table.string('status');
+      table.string('role');
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    }).transacting(ctx);
+  }
+
+  protected async createTenantChatsTable(tenant: Tenant, ctx: any) {
+    await this.database.schema.createTable(tenant.tenant_chats_table, (table) => {
+      table.uuid('id', { primaryKey: true }).defaultTo(this.database.raw('uuid_generate_v4()'));
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    }).transacting(ctx);
+  }
+
+  protected async createTenantMessagesTable(tenant: Tenant, ctx: any) {
+    await this.database.schema.createTable(tenant.tenant_messages_table, (table) => {
+      table.uuid('id', { primaryKey: true }).defaultTo(this.database.raw('uuid_generate_v4()'));
+      table.uuid('participant_id').references(`${tenant.tenant_participants_table}.id`).onDelete('CASCADE');
+      table.uuid('chat_id').references(`${tenant.tenant_chats_table}.id`).onDelete('CASCADE');
+
+      table.text('content');
+      table.date('sent_timestamp');
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    }).transacting(ctx);
+  }
+
+  protected async createTenantMediaTable(tenant: Tenant, ctx: any) {
+    await this.database.schema.createTable(tenant.tenant_media_table, (table) => {
+      table.uuid('id', { primaryKey: true }).defaultTo(this.database.raw('uuid_generate_v4()'));
+      table.string('bucket_path');
+      table.uuid('message_id').references(`${tenant.tenant_messages_table}.id`).onDelete('CASCADE');
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    }).transacting(ctx);
+  }
+
+  protected async createTenantChatMembersTable(tenant: Tenant, ctx: any) {
+    await this.database.schema.createTable(tenant.tenant_chats_members_table, (table) => {
+      table.uuid('id', { primaryKey: true }).defaultTo(this.database.raw('uuid_generate_v4()'));
+      table.uuid('participant_id').references(`${tenant.tenant_participants_table}.id`);
+      table.uuid('user_id').references('users.id').onDelete('CASCADE');
+      table.uuid('chat_id').references(`${tenant.tenant_chats_table}.id`).onDelete('CASCADE');
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    }).transacting(ctx);
+  }
+
+}
+
+class CreateTenant extends CreateTenantTables {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+  constructor(database: Knex<any, unknown[]>) {
+    super(database);
+  }
+
+  async createTenant(tenantDTO: Partial<Tenant>) {
+    await this.database.transaction(async (ctx) => {
+      try {
+        const [tenant]: Tenant[] = await this.database(GlobalTableNames.tenants)
+          .insert(tenantDTO)
+          .returning([
+            'tenant_participants_table',
+            'tenant_chats_table',
+            'tenant_messages_table',
+            'tenant_chats_members_table',
+            'tenant_media_table',
+          ])
+          .transacting(ctx);
+
+        await this.createTenantParticipantsTable(tenant, ctx);
+        await this.createTenantChatsTable(tenant, ctx);
+        await this.createTenantMessagesTable(tenant, ctx);
+        await this.createTenantMediaTable(tenant, ctx);
+        await this.createTenantChatMembersTable(tenant, ctx);
+
+        await ctx.commit();
+      } catch (e) {
+        console.error(e);
+        await ctx.rollback();
+      }
+    });
+  }
+}
+
+export const createTenant = async (userId: string) => {
+  const tenantName = `tenant-${userId}`;
+  const tableNames = getTenantTablesNames(userId);
+
+  const createTenantClient = new CreateTenant(db);
+  await createTenantClient.createTenant({
+    tenant_name: tenantName,
+    tenant_participants_table: tableNames.participants_table,
+    tenant_chats_table: tableNames.chats_table,
+    tenant_messages_table: tableNames.messages_table,
+    tenant_chats_members_table: tableNames.chat_members_table,
+    tenant_media_table: tableNames.media_table,
+  });
+
+  const [newTenant] = await db<Tenant>('tenants').where({ tenant_name: tenantName });
+  return newTenant;
+};
+
