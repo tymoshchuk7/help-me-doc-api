4619060769c325817b28f5713cf1405f0526f274
tymoshchuk7 <tymoshchukalex7@gmail.com>
2025-02-08 13:18:51 +0200

    chore: update 'mark message as read' endpoint

--- app/routes/chats/markMessageAsRead.ts
+++ app/routes/chats/markMessageAsRead.ts
@@ -1,30 +1,12 @@
 import { Response, Request } from 'express';
 import { asyncRoute } from '../../helpers';
-import { GlobalTableNames } from '../../types';
 
 export default asyncRoute(async (req: Request, res: Response) => {
   const { tenant, params: { id } } = req;
 
   const { ChatMessageController } = tenant;
-
   await ChatMessageController.update({ id }, { is_read: true });
-
-  const messageQueryObject = ChatMessageController.query();
-  const messages = await messageQueryObject
-    .join(`${tenant.tenant_chats_members_table} as tcm`, `${tenant.tenant_messages_table}.chat_member_id`, 'tcm.id')
-    .join(`${tenant.tenant_participants_table} as tp`, 'tcm.participant_id', 'tp.id')
-    .join(`${GlobalTableNames.users} as user`, 'tp.user_id', 'user.id')
-    .where(`${tenant.tenant_messages_table}.chat_id`, id)
-    .select(
-      `${tenant.tenant_messages_table}.id`,
-      `${tenant.tenant_messages_table}.chat_id`,
-      `${tenant.tenant_messages_table}.chat_member_id`,
-      `${tenant.tenant_messages_table}.content`,
-      `${tenant.tenant_messages_table}.is_read`,
-      'tp.id as participant_id',
-      'user.id as user_id',
-    )
-    .orderBy(`${tenant.tenant_messages_table}.created_at`, 'asc');
+  const messages = await ChatMessageController.find({ id });
 
   return res.json({ messages });
 });
